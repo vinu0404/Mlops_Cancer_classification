79d2da0c01a2af03940fe52dbb7dcd36778a7100
vinu0404 <22cd3034@rgipt.ac.in>
2024-12-24 17:00:42 +0530

    Folder structure added

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
+project_name="cnn"
+
+file_list=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init.py",
+    f"src/{project_name}/utils/__init.py",
+    f"src/{project_name}/config/__init.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init.py",
+    f"src/{project_name}/entity/__init.py",
+    f"src/{project_name}/constants/__init.py",
+    f"src/{project_name}/config/config.yaml",
+    "dvc.yaml",
+    "setup.py",
+    "params.yaml",
+    "research/trials.ipynb",
+    "templates/inde.html",
+    "main.py",
+    "requirements.txt"
+
+    
+
+]
+
+for filepath in file_list:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"directory created:{filedir} for {filename}")
+
+    if (not os.path.exists(filepath)):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"file  created {filepath}")
+
+    else :
+        logging.info(f" {filename} :files already exist")
+
+    
